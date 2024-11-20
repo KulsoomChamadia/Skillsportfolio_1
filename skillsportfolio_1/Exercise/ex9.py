38180a02a2688f6419663d7df207f37442e6e956
KulsoomChamadia <kulsoomchamadia1@gmail.com>
2024-11-20 13:21:32 +0400

    Completed ex9.py

--- skillsportfolio_1/Exercise/ex9.py
+++ skillsportfolio_1/Exercise/ex9.py
@@ -1,8 +1,8 @@
 def hello():
-    print("Hello")
+    print("Hello")  # Fill in this blank to print "Hello" to the console
     
 def main():
-    hello()
+    hello()  # Fill in this blank to call the hello() function
 
 if __name__ == "__main__":
     main()
