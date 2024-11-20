b6b47e966df2f8e9dcd0f69e88596381bcd19102
KulsoomChamadia <kulsoomchamadia1@gmail.com>
2024-11-20 11:26:23 +0400

    Update ex3.py

--- skillsportfolio_1/Exercise/ex3.py
+++ skillsportfolio_1/Exercise/ex3.py
@@ -1,19 +1,21 @@
 def main():
-    
+    # Define an empty dictionary for storing personal data
     Personal_Data = {}
-    
+    # Inquire about the user's name and hometown
     Personal_Data['Name'] = input(" Enter Your Name (First and Last):")
     Personal_Data['Hometown'] = input(" Enter Your Hometown:")
-    
+    # Authenticate the user's age input
     while True:
         Age_input = input("Enter Your Age: ")
-        
+        # Confirm that the input is a valid integer
         if Age_input.isdigit():
+            # Interpret the input as an integer and save it in the dictionary
             Personal_Data['Age'] = int(Age_input) # Convert to integer
             break 
         else:
+            # Ask the user to provide a valid number for their age
             print("Please Enter Valid Number for Your Age")
-            
+    # Print the results of the data collection       
     print("\n The Data")
     print("\nYour Information:")
     print(f"Name: {Personal_Data['Name']}\nHometown: {Personal_Data['Hometown']}\nAge: {Personal_Data['Age']}")
