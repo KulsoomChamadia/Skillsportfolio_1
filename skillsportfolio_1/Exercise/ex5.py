526bc0d5e8ec5001a4e7888d3ffaddb5d1ebd24d
KulsoomChamadia <kulsoomchamadia1@gmail.com>
2024-11-20 13:00:35 +0400

    Update ex5.py

--- skillsportfolio_1/Exercise/ex5.py
+++ skillsportfolio_1/Exercise/ex5.py
@@ -1,4 +1,4 @@
-
+# create a dictionary of months and their respective number of days
 day_month = {
     "January": 31,
     "February": 28,  
@@ -14,19 +14,21 @@ day_month = {
     "December": 31,
 }
 
-
+# Ask the user to specify the month
 month = input("Enter the {month} ")
-
+# Validate if the input month is acceptable
 if month in day_month:
-    
+    # Check if the year is a leap year for February
     if month == "February":
+        # Ask the user whether it's a leap year
         is_leap_year = input("Is this year a leap year? (yes/no)")
-        
+       # Adjust the number of days in February as needed 
         if is_leap_year.lower() == "yes":
             day_month["February"] = 29
         else:
             day_month["February"] = 28
-            
+   # Output the number of days in the chosen month         
     print(f"There are {day_month[month]} days in {month}")
 else:
+    # Display an error message for invalid month input
     print("Wrong month name.")
